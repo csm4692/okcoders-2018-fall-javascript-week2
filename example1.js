be508d3de4eb4db14ac2e28140466d9398f3b4fe
Carson <csm4692@gmail.com>
2018-10-13 10:40:09 -0500

    For loops while loops and if statments

--- example1.js
+++ example1.js
@@ -21,3 +21,13 @@ while(number >=0) {
    
 }
 
+for (var num = 99; num > 0; num --){
+    console.log(num + " " + "Bottles of Beer on the wall");
+    console.log(num + " " + "Bottles of Beer");
+    console.log("Take one down, pass it around");
+    if((num -1) != 0 ){
+        console.log((num - 1) + " " + "Bottles of beer on the wall");
+    } else {
+         console.log ( " No more bottles of beer on the wall");
+    }
+}
