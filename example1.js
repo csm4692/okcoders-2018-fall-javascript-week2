c6dea7c5f6dbd1b1f2d0c6e8461254283e15cb8c
Carson <csm4692@gmail.com>
2018-10-13 10:17:43 -0500

    First Commit Javascript loop week 2

---
+++ example1.js
@@ -0,0 +1,23 @@
+
+var message = "Bottles of Beer on the wall"
+// console.log(" " + 1message);
+// console.log(message + " " + 2);
+// console.log(message + " " + 3);
+// console.log(message + " " + 4);
+// console.log(message + " " + 5);
+// console.log(message + " " + 6);
+// console.log(message + " " + 7);
+// console.log(message + " " + 8);
+
+var number = 99;
+
+// demonstrate how to use a loop
+
+while(number >=0) {
+    console.log( " " + number + " " + message);
+    // number = number + 1
+    number--; 
+
+   
+}
+
