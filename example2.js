22fe7198b08332c3b308ac0f8e7fad994a4f8393
Carson <csm4692@gmail.com>
2018-10-13 11:51:48 -0500

    adding js examples

---
+++ example2.js
@@ -0,0 +1,29 @@
+// var test = true;
+
+function makemessage (test){
+    if(test){
+    console.log("Evaluated as True");
+} else { 
+    console.log("Evaluated as False")
+    }
+}
+
+
+
+makemessage(true); 
+makemessage(false); 
+makemessage(true); 
+makemessage(false); 
+makemessage(true); 
+makemessage(false); 
+
+console.log("What is the status of: " + test)
+
+
+// for(var num1 = 1; num1 < 46; num1++){
+//     message = ""
+//     for (var num2 = 1; num2 <= num1; num2++){
+//         message = message + num2 + " ";
+//     }
+//     console.log(message);
+// }
